d1abc5483ea6d2ca02dbdbad2692d3d1526866e3
AmritpalBedi <125513113+AmritpalBedi@users.noreply.github.com>
2023-05-15 23:18:17 -0500

    Remove commented out tests in .js file

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,5 @@
 let gridSize = 16;
-// while (true){
-//     gridSize = prompt("Grid size?\n(Between 2-100)")
-//     gridSize = +gridSize;
-//     if (gridSize > 1 && gridSize < 101) {
-//         break;
-//     }
-// }
+
 let drawingArea = document.querySelector("#drawing-area");
 let box;
 for (let i = 0; i < gridSize * gridSize; i++) {
