e21b0b2a2dadc2e18c74e65e7d680421d142e8b4
AmritpalBedi <125513113+AmritpalBedi@users.noreply.github.com>
2023-05-16 11:43:32 -0500

    Create functioning size and reset buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,44 @@
 let gridSize = 16;
-// while (true){
-//     gridSize = prompt("Grid size?\n(Between 2-100)")
-//     gridSize = +gridSize;
-//     if (gridSize > 1 && gridSize < 101) {
-//         break;
-//     }
-// }
 
-let drawingArea = document.querySelector("#drawing-area");
-let box;
-for (let i = 0; i < gridSize * gridSize; i++) {
-    box = document.createElement("div");
-    box.setAttribute("id", "box")
-    box.setAttribute("onmouseover", "mouseOver(this)")
-    //box.style['width'] = `${600 / gridSize}px`;
-    box.style.width = `${600 / gridSize}px`;
-    drawingArea.appendChild(box);
+function createBoard() {
+    const tablet = document.querySelector("#tablet");
+    const drawingArea = document.createElement("div");
+    drawingArea.setAttribute("id", "drawingArea");
+    tablet.appendChild(drawingArea);
     
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box")
+        box.setAttribute("id", "box")
+        box.setAttribute("onmouseover", "mouseOver(this)")
+        box.style.width = `${600 / gridSize}px`;
+        box.style['background-color'] = "white";
+        drawingArea.appendChild(box);
+    }
 }
+
+createBoard();
 
 function mouseOver(element) {
-    element.style['background-color'] = "blue";
+    element.style['background-color'] = "black";
 }
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+    const element = document.getElementsByClassName("box");
+    drawingArea.remove();
+    createBoard();
+});
+
+const chooseSize = document.getElementById("size");
+chooseSize.addEventListener("click", () => {
+    while (true) {
+        gridSize = prompt("Grid size?\n(Between 2-100)")
+        gridSize = +gridSize;
+        if (gridSize > 1 && gridSize < 101) {
+            break;
+        }
+    }
+    drawingArea.remove();
+    createBoard();
+});
