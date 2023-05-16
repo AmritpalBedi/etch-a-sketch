bbc7be428ca61e91063caa61e652eda178fe21d0
AmritpalBedi <125513113+AmritpalBedi@users.noreply.github.com>
2023-05-16 10:36:19 -0500

    Ability to draw in the drawing area

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,23 @@
 let gridSize = 16;
+// while (true){
+//     gridSize = prompt("Grid size?\n(Between 2-100)")
+//     gridSize = +gridSize;
+//     if (gridSize > 1 && gridSize < 101) {
+//         break;
+//     }
+// }
 
 let drawingArea = document.querySelector("#drawing-area");
 let box;
 for (let i = 0; i < gridSize * gridSize; i++) {
     box = document.createElement("div");
-    box.classList.add("box");
+    box.setAttribute("id", "box")
+    box.setAttribute("onmouseover", "mouseOver(this)")
+    //box.style.width = `${600 / gridSize}`;
     drawingArea.appendChild(box);
-    box.setAttribute("style",`width:${600 / gridSize}; height:${600 / gridSize}`);
+    
 }
 
+function mouseOver(element) {
+    element.style['background-color'] = "blue";
+}
